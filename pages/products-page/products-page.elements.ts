5469199ae73603130886ccd9a65b4a9e6d531370
happylearning17 <pedrolopez@amazon.com>
2024-09-18 17:01:19 +0100

    Create class ProductsPageElements

---
+++ pages/products-page/products-page.elements.ts
@@ -0,0 +1,21 @@
+import { Page } from 'playwright'
+
+export class ProductsPageElements {
+    private page: Page
+
+    constructor(page: Page) {
+        this.page = page
+    }
+
+    get icons() {
+        return {
+            cart: this.page.locator('#shopping_cart_container svg')
+        }
+    }
+
+    get buttons() {
+        return {
+            addCart(productName: string){return this.page.locator(`//div[.="${productName}"]//ancestor::div[@class="inventory_item"]//button`)}
+        }
+    }
+}
