114eba6faf15341a17dbcc01b96fa146a0cef6ae
happylearning17 <pedrolopez@amazon.com>
2024-09-19 13:20:23 +0100

    create class CheckoutPageElements

---
+++ pages/checkout-page/checkout-page.elements.ts
@@ -0,0 +1,23 @@
+import {Page} from 'playwright'
+
+export class CheckoutPageElements{
+    private page: Page
+
+    constructor(page: Page){
+        this.page = page
+    }
+
+    get inputs(){
+        return{
+            firstName: this.page.locator('#first-name'),
+            lastName: this.page.locator('#last-name'),
+            postalCode: this.page.locator('#postal-code')
+        }
+    }
+    get buttons(){
+        return{
+            cancel: this.page.locator('.cart_cancel_link'),
+            continue: this.page.locator('.cart_cancel_link')
+        }
+    }
+}
