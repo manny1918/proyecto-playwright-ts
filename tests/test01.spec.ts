b7aca7834366f38f4134fc04aef542904bd0286c
happylearning17 <pedrolopez@amazon.com>
2024-09-19 13:28:41 +0100

    create class checkoutPageMethods

--- tests/test01.spec.ts
+++ tests/test01.spec.ts
@@ -3,6 +3,7 @@ import { CommonPageMethods } from '../pages/common-page/common-page.methods'
 import { LoginPageMethods } from '../pages/login-page/login-page.methods'
 import { LoginPageData } from '../pages/login-page/login-page.data'
 import { ProductsPageMethods } from '../pages/products-page/products-page.methods';
+import { CartPageMethods } from '../pages/cart-page/cart-page.methods';
 
 const userCredentials = LoginPageData.credentials;
 
@@ -10,6 +11,7 @@ test('Lgin', async ({ page }) => {
     const commonPageMethods = new CommonPageMethods(page)
     const loginPageMethods = new LoginPageMethods(page)
     const productPageMethods = new ProductsPageMethods(page)
+    const cartPageMethods = new CartPageMethods(page)
 
     await commonPageMethods.navigateToTheApplication()
     await loginPageMethods.insertUsername(userCredentials.usernames.standardUser)
@@ -17,6 +19,10 @@ test('Lgin', async ({ page }) => {
     await loginPageMethods.clickOnLoginButton()
     await productPageMethods.clickOnAddToCart('Sauce Labs Backpack')
     await productPageMethods.clickOnCartIcon()
+    await cartPageMethods.clickOnCheckoutButton()
+    // await cartPageMethods.clickOnContinueShoppingButton()
+    // await cartPageMethods.clickOnRemoveButton('Sauce Labs Backpack')
+    
     // await commonPageMethods.openMenu()
     await page.waitForTimeout(4000)
 })
